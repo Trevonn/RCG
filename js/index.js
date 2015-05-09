2c4b6beba0df5241e4afd2f0f773c83abf02b7bc
Trevonn David <Trevonn@users.noreply.github.com>
2015-05-09 20:26:58 +0100

    Random Colour Generator 0.1

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var boxes = document.querySelectorAll(".box");
+var labels = document.querySelectorAll(".box-label")
+var button = document.querySelector(".button");
+// Assign references to elements
+
+function random() {
+	for (var i = 0; i < boxes.length; i++) { // For each box
+		var color = [];
+		for (var x = 0; x < 3; x++) {
+			color[x] = Math.floor(Math.random() * 255) + 1;
+			// Generate random colour
+		}
+		boxes[i].style.backgroundColor = "rgb(" + color.toString() + ")";
+		// Assign colour to current box.
+		labels[i].textContent = "rgb(" + color.toString() + ")";
+		// Assign colour name to current box label.
+	}
+	button.textContent = "Refresh";
+}
+
+button.addEventListener("click", random, false);
