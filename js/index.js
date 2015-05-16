d4678418916bbf4a980d7fbc8e7baa485fc1991a
Trevonn David <Trevonn@users.noreply.github.com>
2015-05-16 16:10:42 +0100

    Random Colour Generator 0.41
    Simplified math & removed unused code

--- js/index.js
+++ js/index.js
@@ -6,9 +6,12 @@ var colours = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
 // Assign references to elements
 
 function random(type) {
+	if (type === "rgb") { // rgb colour generator
 		for (var i = 0; i < boxes.length; i++) { // For each box
 			var color = [];
 			for (var x = 0; x < 3; x++) {
+				color[x] = Math.floor(Math.random() * 256);
+				// Generate 3 rgb values and append them to an array
 			}
 			boxes[i].style.backgroundColor = "rgb(" + color.toString() + ")";
 			// Assign colour to current box.
@@ -16,14 +19,16 @@ function random(type) {
 			// Assign colour name to current box label.
 		}
 	}
+	else if (type === "hex") { // hexadecimal colour generator
 		for (var i = 0; i < boxes.length; i++) { // For each box
 			var color = "";
 			for (var x = 0; x < 6; x++) {
-				color += colours[Math.floor(Math.random() * (16 - 0)) + 0].toString();
+				color += colours[Math.floor(Math.random() * 16)].toString();
+				// Generate 6 hexadecimal values
 			}
-			boxes[i].style.backgroundColor = "#" + color.toString();
+			boxes[i].style.backgroundColor = "#" + color;
 			// Assign colour to current box.
-			labels[i].textContent = "#" + color.toString();
+			labels[i].textContent = "#" + color;
 			// Assign colour name to current box label.
 		}
 	}
