03f7ec3746ff26a1ca0f97878ee79d3ccfb1b327
Trevonn David <Trevonn@users.noreply.github.com>
2015-05-16 15:56:05 +0100

    Random Colour Generator 0.4
    You can now choose between hexadecimal and RGB colours

--- js/index.js
+++ js/index.js
@@ -1,22 +1,40 @@
 var boxes = document.querySelectorAll(".box");
 var labels = document.querySelectorAll(".box-label")
-var button = document.querySelector(".button");
+var btn_rgb = document.querySelector(".btn-rgb");
+var btn_hex = document.querySelector(".btn-hex");
+var colours = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
 // Assign references to elements
 
-function random() {
-	for (var i = 0; i < boxes.length; i++) { // For each box
-		var color = [];
-		for (var x = 0; x < 3; x++) {
-			color[x] = Math.floor(Math.random() * 255) + 1;
-			// Generate random colour
+function random(type) {
+		for (var i = 0; i < boxes.length; i++) { // For each box
+			var color = [];
+			for (var x = 0; x < 3; x++) {
+			}
+			boxes[i].style.backgroundColor = "rgb(" + color.toString() + ")";
+			// Assign colour to current box.
+			labels[i].textContent = "rgb(" + color.toString() + ")";
+			// Assign colour name to current box label.
 		}
-		boxes[i].style.backgroundColor = "rgb(" + color.toString() + ")";
-		// Assign colour to current box.
-		labels[i].textContent = "rgb(" + color.toString() + ")";
-		// Assign colour name to current box label.
+	}
+		for (var i = 0; i < boxes.length; i++) { // For each box
+			var color = "";
+			for (var x = 0; x < 6; x++) {
+				color += colours[Math.floor(Math.random() * (16 - 0)) + 0].toString();
+			}
+			boxes[i].style.backgroundColor = "#" + color.toString();
+			// Assign colour to current box.
+			labels[i].textContent = "#" + color.toString();
+			// Assign colour name to current box label.
+		}
 	}
 }
 
-random();
+btn_rgb.addEventListener("click", function() {
+	random("rgb");
+}, false); // Add function call to RGB button
 
-button.addEventListener("click", random, false);
+btn_hex.addEventListener("click", function() {
+	random("hex");
+}, false); // Add function call to hexadecimal button
+
+random("hex");
