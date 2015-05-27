70336a7d31d97627fc579e3bed4473541eb2626d
Trevonn David <Trevonn@users.noreply.github.com>
2015-05-27 19:35:01 +0100

    Comment lines and renaming variables

--- js/rcg.js
+++ js/rcg.js
@@ -1,4 +1,4 @@
-var colours = "0123456789ABCDEF",
+var hex_alpha = "0123456789ABCDEF",
 	current = "",
 	default_rgb_min = 0,
 	default_rgb_max = 255,
@@ -25,7 +25,7 @@ function rcg(type, min, max) {
 	} else if (type === "hex") { // hexadecimal colour generator
 		var color = ""; // Create color string
 		for (var x = 0; x < 6; x++) { // Loop six times
-			color += colours.charAt(
+			color += hex_alpha.charAt(
 				Math.floor(Math.random() * 16)
 			); // Select random hexadecimal value
 		}
@@ -33,46 +33,62 @@ function rcg(type, min, max) {
 	}
 }
 
-function convert(type, input_color) {
+function convert(type, colorInput) {
+
 	if (type === "hex") { // Convert hexadecimal colour to RGB colour
-		var color = [];
+		var colorArray = [];
 
-		color[0] = parseInt("0x" + input_color.substring(1,3), 16);
+		colorArray[0] = parseInt("0x" + colorInput.substring(1,3), 16);
 		// Red
-		color[1] = parseInt("0x" + input_color.substring(3,5), 16);
+		colorArray[1] = parseInt("0x" + colorInput.substring(3,5), 16);
 		// Green
-		color[2] = parseInt("0x" + input_color.substring(5,7), 16);
+		colorArray[2] = parseInt("0x" + colorInput.substring(5,7), 16);
 		// Blue
+		console.log("Successfully converted a " + type +" colour");
 
-		return "rgb(" + color.toString() + ")";
+		return "rgb(" + colorArray.toString() + ")";
 	}
+
 	if (type === "rgb") { // Convert RGB Colour to hexadecimal colour
-		var numbers = [],
-			c = 0,
-			currentColor = "",
-			color = "#";
-		for (var i = 0; i < input_color.length; i++) {
+		var	position = 0,
+			color = "#",
+			colorArray = [],
+			currentColor = "";
+			// Declare variables
 
-			var char = input_color.charAt(i);
-			var isNum = isNaN(parseInt(char), 10);
+		for (var i = 0; i < colorInput.length; i++) {
+			// Loop through colour string
 
-			if (isNum === false) {
-				currentColor += char;
-				numbers[c] = currentColor;
+			var currentChar = colorInput.charAt(i);
+			// Assign current position in the colour string to variable
+
+			var isNum = isNaN(parseInt(currentChar), 10);
+			// Determine whether the current character is a number or not
+
+			if (!isNum) { // if the current character is a number
+				currentColor += currentChar;
+				colorArray[position] = currentColor; // Add colour to array
 			}
-			if (char === ",") {
-				numbers[c] = currentColor;
-				c++;
-				currentColor = "";
+			if (currentChar === ",") {
+				colorArray[position] = currentColor; // Add colour to array
+				position++; // Move on to next colour
+				currentColor = ""; // Delete current colour
 			}
 		}
-		if (numbers.length === 3) {
-			for (var i = 0; i < 3; i++) {
-				var num2 = numbers[i];
-				color += colours.charAt( (num2 - num2 % 16) / 16);
-				color += colours.charAt(num2 % 16);
+
+		if (colorArray.length === 3) { // if colour array contains 3 colours
+			for (var i = 0; i < 3; i++) { // loop through colour array
+
+				var colorValue = colorArray[i];
+				// Assing current position to vaiable
+
+				color += hex_alpha.charAt( (colorValue - colorValue % 16) / 16);
+				color += hex_alpha.charAt(colorValue % 16);
+				// Use magic maths to deduce
 			}
 		}
+
+		console.log("Successfully converted a " + type +" colour");
 		return color;
 	}
 }
