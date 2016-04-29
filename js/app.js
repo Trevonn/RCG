68fdec16f351978a2bc8e63d13a09c4b0197e5ab
Trevonn David <trevonn95@gmail.com>
2016-04-29 04:16:47 +0100

    Switch to Standard Style JavaScript
    Also invoke strict mode properly

--- js/app.js
+++ js/app.js
@@ -1,133 +1,128 @@
-var boxes = document.querySelectorAll('.inner-box');
-var boxLabels = document.querySelectorAll('.box-label');
-var inputRGB = document.getElementsByClassName('choice')[0];
-var inputHEX = document.getElementsByClassName('choice')[1];
-var btnType = document.querySelectorAll('.btn-type');
-var btnRefresh = document.querySelector('.btn-refresh');
-var copyright = document.querySelector('.copyright-js');
-var inputs = document.querySelectorAll('.colorInput');
-var spinner = document.querySelector('.fa-refresh');
-var curType = 'rgb';
-var colors = [];
-var min = 0;
-var max = 255;
+'use strict' // Strict Mode
 
-year = new Date();
+var boxes = document.querySelectorAll('.inner-box')
+var boxLabels = document.querySelectorAll('.box-label')
+var inputRGB = document.getElementsByClassName('choice')[0]
+var inputHEX = document.getElementsByClassName('choice')[1]
+var btnType = document.querySelectorAll('.btn-type')
+var btnRefresh = document.querySelector('.btn-refresh')
+var copyright = document.querySelector('.copyright-js')
+var inputs = document.querySelectorAll('.colorInput')
+var spinner = document.querySelector('.fa-refresh')
+var curType = 'rgb'
+var colors = []
+var min = 0
+var max = 255
+var year = new Date()
 
 // Declare varibales
 
-function filterInt(value) {
-  if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value))
-    return Number(value);
-  return NaN;
+function filterInt (value) {
+  if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value)) {
+    return Number(value)
+  }
+  return NaN
 }
 
-function refresh(elements, labels, min, max) {
-  'use strict';
-
+function refresh (elements, labels, min, max) {
   if (curType === 'rgb') {
-    var customRgbMin = filterInt(inputs[0].value);
-    var customRgbMax = filterInt(inputs[1].value);
+    var customRgbMin = filterInt(inputs[0].value)
+    var customRgbMax = filterInt(inputs[1].value)
 
-    console.log(customRgbMin);
-    console.log(customRgbMax);
+    console.log(customRgbMin)
+    console.log(customRgbMax)
 
     if (customRgbMin < 0 || customRgbMin > 255 || isNaN(customRgbMin)) {
-      min = 0;
-      inputs[0].value = '0';
+      min = 0
+      inputs[0].value = '0'
     } else {
-      min = customRgbMin;
+      min = customRgbMin
     } // if chosen minimum is invalid reset the value
     if (customRgbMax < 0 || customRgbMax > 255 || isNaN(customRgbMax)) {
-      max = 255;
-      inputs[1].value = '255';
+      max = 255
+      inputs[1].value = '255'
     } else {
-      max = customRgbMax;
+      max = customRgbMax
     } // if chosen maximum is invalid reset the value
 
-    console.log(min);
-    console.log(max);
+    console.log(min)
+    console.log(max)
   } else {
-    var customHexMin = rc.hexes.indexOf(inputs[2].value);
-    var customHexMax = rc.hexes.indexOf(inputs[3].value);
+    var customHexMin = rc.hexes.indexOf(inputs[2].value)
+    var customHexMax = rc.hexes.indexOf(inputs[3].value)
 
     if (customHexMin !== -1) {
-      min = customHexMin;
+      min = customHexMin
     } else {
-      inputs[2].value = '0';
-      min = 0;
+      inputs[2].value = '0'
+      min = 0
     } // if chosen minimum is invalid reset the value
     if (customHexMax !== -1) {
-      max = customHexMax;
+      max = customHexMax
     } else {
-      inputs[3].value = 'F';
-      max = 15;
+      inputs[3].value = 'F'
+      max = 15
     } // if chosen maximum is invalid reset the value
   }
 
-  colors = rc.newColor(curType, min, max, elements.length); // Generate colors
+  colors = rc.newColor(curType, min, max, elements.length) // Generate colors
 
-  for (var i = 0; i < elements.length; i++) {    // For each element
-    var color = rc.toText(colors[i], curType);   // Assign color to variable
-    elements[i].style.backgroundColor = color;   // Change Background color
-    labels[i].textContent = color;               // Change label
+  for (var i = 0; i < elements.length; i++) { // For each element
+    var color = rc.toText(colors[i], curType) // Assign color to variable
+    elements[i].style.backgroundColor = color // Change Background color
+    labels[i].textContent = color // Change label
   }
 }
 
-function changeType(elements, labels, type1) {
-  'use strict';
-
+function changeType (elements, labels, type1) {
   if (curType === type1) {
-    return; // Exit if color type is the same
+    return // Exit if color type is the same
   }
 
   for (var i = 0; i < elements.length; i++) {
-    var converted = rc.convert(type1, colors[i]);
+    var converted = rc.convert(type1, colors[i])
     // Assign converted color to variable
-    var color = rc.toText(converted, type1);
-    labels[i].textContent = color;
+    var color = rc.toText(converted, type1)
+    labels[i].textContent = color
     // Change labels to converted color
-    colors[i] = converted;
-    // Replace color in color array with converted color
+    colors[i] = converted
+  // Replace color in color array with converted color
   }
 
-  curType = type1; // Change current color type
+  curType = type1 // Change current color type
 }
 
 for (var i = 0; i < btnType.length; i++) {
-  btnType[i].addEventListener('click', function() {
-    'use strict';
-    changeType(boxes, boxLabels, this.textContent.toLowerCase());
-  }, false);
+  btnType[i].addEventListener('click', function () {
+    changeType(boxes, boxLabels, this.textContent.toLowerCase())
+  }, false)
 } // Add event listeners to color type buttons
 
-btnRefresh.addEventListener('click', function() {
-  'use strict';
-  refresh(boxes, boxLabels, min, max);
+btnRefresh.addEventListener('click', function () {
+  refresh(boxes, boxLabels, min, max)
   if (curType === 'rgb') {
-    inputRGB.checked = true;
+    inputRGB.checked = true
   } else if (curType === 'hex') {
-    inputHEX.checked = true;
+    inputHEX.checked = true
   }
-  spinner.classList.add('spin');
-  window.setTimeout(function() {
-      spinner.classList.remove('spin');
-    }, 200);
-}, false); // Add function call to refresh button
+  spinner.classList.add('spin')
+  window.setTimeout(function () {
+    spinner.classList.remove('spin')
+  }, 200)
+}, false) // Add function call to refresh button
 
-document.onreadystatechange = function() {
-  'use strict';
-  if (document.readyState == 'complete') {
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
     if (curType === 'rgb') {
-      inputRGB.checked = true;
+      inputRGB.checked = true
     } else if (curType === 'hex') {
-      inputHEX.checked = true;
+      inputHEX.checked = true
     }
   }
-};
+}
 
-year = year.getFullYear();
+year = year.getFullYear()
 
-copyright.innerHTML = '&copy; ' + year + ' - Trevonn';
+copyright.innerHTML = '&copy; ' + year + ' - Trevonn'
 
-refresh(boxes, boxLabels, min, max); // Initialise
+refresh(boxes, boxLabels, min, max) // Initialise
--- js/rc.js
+++ js/rc.js
@@ -1,111 +1,106 @@
+'use strict' // Strict Mode
+
 var rc = {} // Declare object
+rc.hexes = '0123456789abcdef'
 // Assign variables
-rc.hexes = '0123456789abcdef';
 
-rc.newNum = function(min, max) {
-  'use strict';
-  return Math.floor(Math.random() * (max + 1 - min) + min);
-}
+rc.newNum = function (min, max) {
+  return Math.floor(Math.random() * (max + 1 - min) + min)
+} // Returns a number between a specified minimum and maximum
 
-rc.newColor = function(type, min, max, amount) {
-  'use strict';
-  var output = [];
+rc.newColor = function (type, min, max, amount) {
+  var output = []
 
   if (amount === undefined || amount < 1 || amount === null) {
-    amount = 1;
+    amount = 1
   }
 
   if (type === '' || type === undefined) {
-    type = 'rgb';
+    type = 'rgb'
   }
 
   if (type === 'rgb') {
     if (min === undefined || min < 0 || min > 255 || isNaN(min) || min > max) {
-      min = 0;
+      min = 0
     }
 
     if (max === undefined || max > 255 || max < 0 || isNaN(max) || max < min) {
-      max = 255;
+      max = 255
     }
   } else if (type === 'hex') {
     if (min === undefined || min < 0 || min > 15 || min > max) {
-      min = 0;
+      min = 0
     }
     if (max === undefined || max > 15 || max < 0 || max < min) {
-      max = 15;
+      max = 15
     }
   } // if min or max are empty or do not fit within boundaries use default
 
   for (var i = 0; i < amount; i++) {
-    var color = [];
+    var color = []
 
     for (var x = 0; x < 3; x++) {
       if (type === 'rgb') {
-        color[x] = this.newNum(min, max);
+        color[x] = this.newNum(min, max)
       } // Add random RGB color to array
 
       if (type === 'hex') {
         color[x] = this.hexes.charAt(this.newNum(min, max)) +
-                   this.hexes.charAt(this.newNum(min, max));
+        this.hexes.charAt(this.newNum(min, max))
       } // Add random hex color to array
     }
 
-    output[i] = color;
-    color = [];
+    output[i] = color
+    color = []
   }
-  return output;
+  return output
 }
 
-rc.convert = function(type, input) {
-  'use strict';
-  var color = [];
+rc.convert = function (type, input) {
+  var color = []
 
   for (var i = 0; i < input.length; i++) {
     switch (type) {
-
       case 'rgb':
-        color[i] = parseInt('0x' + input[i], 16);
-        break;
+        color[i] = parseInt('0x' + input[i], 16)
+        break
 
       case 'hex':
-        color[i] = this.hexes.charAt((input[i] - input[i] % 16) / 16) +
-                   this.hexes.charAt(input[i] % 16);
-        break;
+        color[i] = this.hexes.charAt((input[i] - input[i] % 16) / 16) + this.hexes.charAt(input[i] % 16)
+        break
 
       default:
-        console.log('Function: Convert \nError: color type is invalid. \nTry putting quotes around the string.');
-        break;
+        console.log('Function: Convert \nError: color type is invalid. \nTry putting quotes around the string.')
+        break
     }
   }
-  return color; // Spit out converted color
+  return color // Spit out converted color
 }
 
-rc.toText = function(color, type) {
+rc.toText = function (color, type) {
   /*
     This function takes color values from an array and places them inside
     a readable string of text.
   */
-  'use strict';
-  var result;
+  var result
 
   switch (type) {
-
     case 'rgb':
-      result = 'rgb(' + color.toString() + ')';
-      break;
+      result = 'rgb(' + color.toString() + ')'
+      break
 
     case 'hex':
-      result = '#' + color[0] + color[1] + color[2];
-      result = result.toUpperCase();
-      break;
+      result = '#' + color[0] + color[1] + color[2]
+      result = result.toUpperCase()
+      break
 
     case undefined:
-      result = 'rgb(' + color.toString() + ')';
-      break;
+      result = 'rgb(' + color.toString() + ')'
+      break
 
     default:
-      console.log('Function: toText \nError: Color type is invalid. \nFix: The color type must be either hex or rgb');
-      break;
+      console.log('Function: toText \nError: Color type is invalid. \nFix: The color type must be either hex or rgb')
+      break
   }
-  return result;
+  return result
 }
