fbb0ee153e657c092278f6f6a421a54284a0015b
Trevonn <trevonn95@gmail.com>
2016-06-21 12:12:56 +0100

    Minor edits

--- js/app.js
+++ js/app.js
@@ -25,14 +25,14 @@ var year = new Date()
 
 // Declare varibales
 
-function filterInt (value) {
+function filterInt(value) {
   if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value)) {
     return Number(value)
   }
   return NaN
 }
 
-function refresh (elements, labels, min, max) {
+function refresh(elements, labels, min, max) {
   if (curType === 'rgb') {
     var customRgbMin = filterInt(inputs[0].value) // RGB Min
     var customRgbMax = filterInt(inputs[1].value) // RGB Max
@@ -42,15 +42,15 @@ function refresh (elements, labels, min, max) {
       inputs[0].value = '0'
     } else {
       min = customRgbMin
-    } 
+    }
     // if chosen minimum is invalid reset the input value
     if (customRgbMax < 0 || customRgbMax > 255 || isNaN(customRgbMax)) {
       max = 255
       inputs[1].value = '255'
     } else {
       max = customRgbMax
-    } 
-    // if chosen maximum is invalid reset the value
+    }
+  // if chosen maximum is invalid reset the value
   } else {
     var customHexMin = rc.hexes.indexOf(inputs[2].value) // Hex Min
     var customHexMax = rc.hexes.indexOf(inputs[3].value) // Hex Max
@@ -78,7 +78,7 @@ function refresh (elements, labels, min, max) {
   }
 }
 
-function changeType (elements, labels, type1) {
+function changeType(elements, labels, type1) {
   if (curType === type1) {
     return // Exit if color type is the same
   }
@@ -97,12 +97,12 @@ function changeType (elements, labels, type1) {
 }
 
 for (var i = 0; i < btnType.length; i++) {
-  btnType[i].addEventListener('click', function () {
+  btnType[i].addEventListener('click', function() {
     changeType(boxes, boxLabels, this.textContent.toLowerCase())
   }, false)
 } // Add event listeners to color type buttons
 
-btnRefresh.addEventListener('click', function () {
+btnRefresh.addEventListener('click', function() {
   refresh(boxes, boxLabels, min, max)
   if (curType === 'rgb') {
     inputRGB.checked = true
@@ -110,12 +110,12 @@ btnRefresh.addEventListener('click', function () {
     inputHEX.checked = true
   }
   spinner.classList.add('spin')
-  window.setTimeout(function () {
+  window.setTimeout(function() {
     spinner.classList.remove('spin')
   }, 200)
 }, false) // Add function call to refresh button
 
-document.onreadystatechange = function () {
+document.onreadystatechange = function() {
   if (document.readyState === 'complete') {
     if (curType === 'rgb') {
       inputRGB.checked = true
